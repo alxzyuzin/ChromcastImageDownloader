207db0e5b6d615d29e4a05e5b39cb128117f3f37
Alexander Zyuzin <alx.zyuzin@gmail.com>
2023-01-13 11:40:58 -0800

    Add copyrite headers to program files

--- grabber/__main__.py
+++ grabber/__main__.py
@@ -1,3 +1,10 @@
 
+#-----------------------------------------------------------------------
+# Copyright (c) 2023, Alexander Zyuzin.
+#
+# This file is part of  Chromcast Image Downloader.
+#
+# --------------------------------------------------------------------------
+
 from utils import parseArgsAndStart
 parseArgsAndStart()
