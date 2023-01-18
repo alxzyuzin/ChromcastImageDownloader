e2eb6b62d9450cf85b4a849358108b2ca74a79f3
Alexander Zyuzin <alx.zyuzin@gmail.com>
2023-01-17 16:27:19 -0800

    Small bugs fixed

--- imagegrabber/lib.py
+++ imagegrabber/lib.py
@@ -99,6 +99,7 @@ class grabber:
         parser.add_argument("-m", action="store_true", help = "Paste metadata into filenames of saved images")
         parser.add_argument("-n", type=int, default = "10", help = "Number of new images for download (default = 10)")
         parser.add_argument("-p", type=str, default="", help = "Path where to save downloaded images. If omitted images will be saved in directory where script started.")
+        parser.add_argument("-v", type=int, default="0", help = "Verbose level.(default = 0, possible: 0, 1)")
 
         #image size parameters
         #parser.add_argument("-w", type=int, default = "1920", help = "Downloaded image width (default = 1920)")
@@ -108,13 +109,13 @@ class grabber:
         self.target_dir = args.p
         self.n_of_fles = args.n
         self.use_metadata = args.m
+        self.verbose_level = args.v
         td = "Current directory."
         # Remove traling slashes from destination path (target_dir)
         if self.target_dir != "":
             while self.target_dir[len(self.target_dir) - 1] == "\\":
                 self.target_dir = self.target_dir[:len(self.target_dir)-1]
-  
-    def downloadImages(self):
+
         if self.target_dir == "":
             td = "Current directory"
         else:
@@ -123,13 +124,7 @@ class grabber:
         print("Target directory:{}\nNumber of images to download: {}.".format(td, self.n_of_fles ))
         if self.use_metadata:
             print("Image metadata will be pasted into filenames.\n")
-
-        filesDownloaded = 0
-        print(HIDE_CURSOR, end="")
-        filesDownloaded = self._grabImages()
-        print(SHOW_CURSOR, end = "")
-        return filesDownloaded
-        
+  
     def _extractImageURL(self, webpage):
         '''
         Extract extract image URL from web page
@@ -208,7 +203,7 @@ class grabber:
         import requests
         from requests.exceptions import HTTPError, ConnectTimeout, ConnectionError
         
-        print("File name : {}".format(filename))
+        print("\nFile name : {}".format(filename))
         try:
             img_data = requests.get(img_url).content
         except ConnectTimeout as ct:
@@ -236,7 +231,8 @@ class grabber:
                 print("StrError:{} WinError {}".format(ex.strerror, ex.winerror))
                 return False
 
-    def _grabImages(self):
+    def grabImages(self):
+        print(HIDE_CURSOR, end="")
         imgIDRepeatCounter = 0
         img_ID_2 = None
         retrievedImagesIDs = {}
@@ -253,9 +249,9 @@ class grabber:
                 metadata = self._getMetadataLine2()
                 if len(metadata) > 0:
                     metadata += "_"
-           
-            print("\nImage url : {}.".format(image_url))
-            print("Image metadata : {}.".format( metadata))
+            if self.verbose_level > 0:
+                print("\nImage url : {}.".format(image_url))
+                print("Image metadata : {}.".format( metadata))
             # Check if file already downloaded and saved
             img_ID = image_url.split("/")[-1]
             if img_ID not in retrievedImagesIDs:  # if file with this ID was not downloade and saved      
@@ -266,6 +262,7 @@ class grabber:
                     imageGrabbed+=1
                     fnumber+=1
                     self.retrievedImagesIDs[img_ID] = filename
+                    print("Total image downloaded {} of {}".format(imageGrabbed, self.n_of_fles))
                     # Check if max try of downloading the same image exided or not            
                     if img_ID_2 == img_ID:
                         imgIDRepeatCounter += 1
@@ -279,5 +276,5 @@ class grabber:
                     print("Check network connection.")
                     print("Application exited.")
                     break
-        
+        print(SHOW_CURSOR, end = "")
         return imageGrabbed
--- main2.py
+++ main2.py
@@ -6,7 +6,7 @@ from imagegrabber.lib import grabber
 grb = grabber()
 grb.open(BASE_URL)
 startTime = time.time()
-number_of_files_downloaded = grb.downloadImages()
+number_of_files_downloaded = grb.grabImages()
 print("Requested number of files reached.")
 grb.close()
 print("{} files downloaded\n.".format(number_of_files_downloaded))
